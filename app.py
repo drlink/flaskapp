fa91abd9e74b4305d650715814a4e7ecf4030046
drlink <cezar.rossi@gmail.com>
2022-07-19 01:41:32 -0300

    inserção de form no sistema

--- app.py
+++ app.py
@@ -1,17 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 
 app = Flask(__name__) #importa a classe Flask e define o parametro name dentro da variavel
-@app.route('/')
+
+#Recebimento de dados variavel global
+fruta2 = [] #variavel global para que o seu conteu não seja modificado a cada carregamento
+@app.route('/', methods=["GET", "POST"])
 # 127.0.0.1:5000
 def princ():
+    #Envio de dados
     nome = "Sr y"
     idade = 39
     feira = ["Frutas", "verdura" , "legumes", "carnes"]
     frutas = ["Morango", "Uva", "Laranja", "Mamão", "Maça","Melão","Abacaxi","pera"]
     notas = {"Aluno1": 5.0, "Aluno2": 6.0, "Aluno3": 7.0, "Aluno4": 8.5}
-
-    return render_template("index.html", nome=nome, idade=idade, feira=feira, frutas=frutas, notas=notas)
+    #Recebendo dados - prestar atenção o nome do que se refcebe e o nome do input e não da variavel
+    if request.method == "POST":
+        if request.form.get("input_form"):
+            fruta2.append(request.form.get("input_form"))
+    return render_template("index.html", nome=nome, idade=idade, feira=feira, frutas=frutas, notas=notas, fruta2=fruta2)
 
 @app.route('/sobre')
 def sobre():
