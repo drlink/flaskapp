5781b00cb8816d640989c3138d2af3be8c5ce173
drlink <cezar.rossi@gmail.com>
2022-07-25 12:53:31 -0300

    atualização inserção de comunicação para api

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ app = Flask(__name__) #importa a classe Flask e define o parametro name dentro d
 
 #Recebimento de dados variavel global
 fruta2 = [] #variavel global para que o seu conteu não seja modificado a cada carregamento
+registros = []
 @app.route('/', methods=["GET", "POST"])
 # 127.0.0.1:5000
 def princ():
@@ -20,6 +21,12 @@ def princ():
             fruta2.append(request.form.get("input_form"))
     return render_template("index.html", nome=nome, idade=idade, feira=feira, frutas=frutas, notas=notas, fruta2=fruta2)
 
-@app.route('/sobre')
+
+@app.route('/sobre', methods=["GET", "POST"])
 def sobre():
-    return render_template('sobre.html')
+    # Recebendo dados - prestar atenção o nome do que se recebe e o nome do input e não da variavel
+    if request.method == "POST":
+        if request.form.get("aluno") and request.form.get("nota"):
+            registros.append({"aluno": request.form.get("aluno"), "nota": request.form.get("nota")})
+    return render_template("sobre.html", registros=registros)
+
