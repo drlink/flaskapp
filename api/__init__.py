8a06d326f80998203205176fe4a62e131bef23fa
drlink <cezar.rossi@gmail.com>
2022-07-28 23:36:06 -0300

    Adicionando um validador de dados ao projeto

--- api/__init__.py
+++ api/__init__.py
@@ -2,13 +2,15 @@ from flask import Flask
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_marshmallow import Marshmallow
 import pymysql #correção do erro das migration sem isso as migrations não rodam
-pymysql.install_as_MySQLdb()
+pymysql.install_as_MySQLdb() # Codigo responsavel por instalar e linkar o MYSQLdb ao executa a migration
 
 # Configurações iniciais
 app = Flask(__name__) #configuração do app
 app.config.from_object('config')#chama arquivo de configuração
 db =SQLAlchemy(app) #configura o banco de dados
+ma =Marshmallow(app) #configura a classe de validação de dados
 migrate = Migrate(app, db) #configura as migrates
 api = Api(app) # configuração da api
 ###
